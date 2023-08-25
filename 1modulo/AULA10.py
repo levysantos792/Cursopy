1ee1ff9b408cc4e35497056b16205b78101c53ac
unknown <leonardo@software-ltda.com.br>
2023-08-25 10:27:33 -0300

    aulas 9 e 10

---
+++ 1modulo/AULA10.py
@@ -0,0 +1,11 @@
+##CONCATENAÇÃO E REPETIÇÃO##
+
+concatenacao = 'a' + 'b' + 'c'
+
+repeticao_3_vezes = 3 * 'levi '
+repeticao_10_vezes = 10 * 'santos '
+
+# print('concatenacao= ' + concatenacao)
+
+print(repeticao_3_vezes)
+print(repeticao_10_vezes)
---
+++ 1modulo/aula9.py
@@ -0,0 +1,14 @@
+##OPERADORES MATEMÁTICOS##
+
+
+divisao_normal = 10 / 2.2
+print('DIVISAO NOMARL: ', divisao_normal)
+
+divisao_inteira = 10 // 2.2
+print('DIVISAO INTEIRA: ', divisao_inteira)
+
+exponeciacao = 2 ** 10 #2 elevado a 10
+print('POTENCIA: ', exponeciacao)
+
+modulo = 55 % 2 # resto da divsao entre 55 e 2
+print('RESTO DA DIVISAO: ', modulo)
